9f6a2a8321192293818b4aefbf21d30ae421532d
Sunil Pai <spai@cloudflare.com>
2025-02-25 12:14:02 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -91,6 +91,12 @@ export class Chat extends AIChatAgent<Env> {
  */
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    if (!env.OPENAI_API_KEY) {
+      console.error(
+        "OPENAI_API_KEY is not set, don't forget to set it locally in .dev.vars, and use `wrangler secret bulk .dev.vars` to upload it to production"
+      );
+      return new Response("OPENAI_API_KEY is not set", { status: 500 });
+    }
     return (
       // Route the request to our agent or return 404 if not found
       (await routeAgentRequest(request, env)) ||
