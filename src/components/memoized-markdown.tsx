74745189465d70f1709bed83bfa9e51d52907abb
György Márk Varga <gyorgy.varga@shiwaforce.com>
2025-04-28 16:39:29 +0200

    added plugin to display tables and other complex markdown stuff

--- src/components/memoized-markdown.tsx
+++ src/components/memoized-markdown.tsx
@@ -2,6 +2,7 @@ import { marked } from 'marked';
 import type { Tokens } from 'marked';
 import { memo, useMemo } from 'react';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 function parseMarkdownIntoBlocks(markdown: string): string[] {
   const tokens: TokensList = marked.lexer(markdown);
@@ -11,7 +12,11 @@ function parseMarkdownIntoBlocks(markdown: string): string[] {
 type TokensList = Array<Tokens.Generic & { raw: string }>;
 
 const MemoizedMarkdownBlock = memo(
-  ({ content }: { content: string }) => <ReactMarkdown>{content}</ReactMarkdown>,
+  ({ content }: { content: string }) => (
+    <div className="markdown-body">
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+    </div>
+  ),
   (prevProps, nextProps) => prevProps.content === nextProps.content
 );
 
